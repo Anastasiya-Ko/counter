daaf41682e166506e63f499251692bcc75aa5f2b
Anastasiya <agropina_90@mail.ru>
2023-11-12 12:52:22 +0300

    написал метод сервиса для подсчёта количества повторения символов с строке

--- src/main/java/kopylva/counter/services/CounterService.java
+++ src/main/java/kopylva/counter/services/CounterService.java
@@ -2,7 +2,29 @@ package kopylva.counter.services;
 
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 @Service
 public class CounterService {
+    public String read(String text) {
 
+        if (text == null) {
+            throw new RuntimeException("Строка не должна быть пустой");
+        }
+
+
+        Map<String, Integer> result = new HashMap<>();
+
+        text.chars()
+                .mapToObj(e -> (char) e)
+                .collect(Collectors.groupingBy(Character::charValue))
+                .forEach((key, value) -> result.put(String.valueOf(key), value.size()));
+
+        return result.entrySet().stream()
+                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+                .map(e -> String.format("\"%s\": %s", e.getKey(), e.getValue()))
+                .collect(Collectors.joining(", "));
+    }
 }
